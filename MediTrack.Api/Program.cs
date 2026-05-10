eb516244ab3a382e0507a4a0b2160e273b0fbd9c
sNirajan <snirajan212@gmail.com>
2026-05-10 10:37:09 -0500

    configured EF Core with SQL Server Provider

--- MediTrack.Api/Program.cs
+++ MediTrack.Api/Program.cs
@@ -1,23 +1,40 @@
+using MediTrack.Api.Data; // lets program.cs recognize ApplicationDbContext
+using Microsoft.EntityFrameworkCore; // lets program.cs use EF core methods 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// This tells Asp.Net core that we are using controller classes
+// Later, this allows routes like /api/patients or /api/appointments to work
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
+// This registers our database context with the app.
+// ApplicationDbContext is our bridge between C# models and the database.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    // This reads "DefaultConnection" from appsettings.Development.json
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    // This tells EF core to use SQLite as the database for local development
+    options.UseSqlServer(connectionString);
+});
+
+// This builds the actual web application
+// Before this line, we are only registering/configuring services
+// After this line, we configure how request move through the app
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Simple test route so we can quickly check if the API is alive
+// When we visit http://localhost:5126/, it should show this message
+app.MapGet("/", () => "MediTrack API is running");
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+// This connects controller routes to the app
+// Without this, controller endpoints will not work
 app.MapControllers();
 
+// This starts the web server
 app.Run();
+
+
+
+
